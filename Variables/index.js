1c7520513abd1c53e9a1efab74ebb6dc4ab0feae
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-11-16 14:10:58 +0530

    variables

---
+++ Variables/index.js
@@ -0,0 +1,55 @@
+console.log(`Hello`);
+console.log(`I like Pizza!`);
+
+// ---------------------------------------------------------------
+// Display Alert
+// window.alert(`This is an alert!`);
+// window.alert(`I like Pizza!`);
+
+// ---------------------------------------------------------------
+// Change or add the element content text
+document.getElementById("head1").textContent = `Hello World`;
+document.getElementById("p1").textContent = `I like ice cream`;
+
+// ----------------------------------------------------------------
+// Variable = A container that stores a value
+//          = Behaves as if it were the value it contain
+
+// 1. declaration -> let x;
+// 2. assignment -> x = 100;
+
+// Number Data Types
+    let age = 25;
+    let price = 10.99;
+    let GPA = 2.4;
+
+    console.log(`Your age is ${age} years old`);
+    console.log(`The price is $${price}`);
+    console.log(`Your GPA is $(GPA)`);
+    // Find data types -> typeOf()
+    console.log(typeof GPA);
+
+// String Data Types
+    let firstName = "Sachinda"
+    let lastName = "Bandara"
+    let email = "sachinda@gmail.com"
+
+    console.log(`My name is ${firstName} ${lastName}`);
+    console.log(`My email is ${email}`);
+    // Find data types -> typeOf()
+    console.log(typeof email);
+
+// Boolean Data Type
+    let online = true;
+    console.log(`Chamindu is online: ${online}`);
+    // Find data types -> typeOf()
+    console.log(typeof online); 
+
+// Example
+    let fullName ="Sachinda Bandara"
+    let studentAge = 25;
+    let isStudent = false;
+
+    document.getElementById("p_1").textContent = `Your Name is ${fullName}`;
+    document.getElementById("p_2").textContent = `You are ${studentAge} years old`;
+    document.getElementById("p_3").textContent = `Enrolled : ${isStudent}`;
