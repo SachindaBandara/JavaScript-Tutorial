5b5b30a652b105635ce42430d54e8540c7048ecf
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-11-18 17:26:37 +0530

    use Math method

---
+++ Math/index.js
@@ -0,0 +1,32 @@
+// Math = built-in object that provides a collection of properties and modules
+
+// let x = 3.25;
+// let y = 2;
+
+// let k = 25;
+// let m = -2;
+// let n = 1;
+
+// let z;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x, y);
+// z = Math.sqrt(k);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(m);
+// z = Math.sign(x);
+
+// console.log(z);
+
+
+// let max = Math.max(k, m, n);
+// console.log(max);
+
+// let min = Math.min(k, m, n);
+// console.log(min);
