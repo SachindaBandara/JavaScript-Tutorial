ac9d6d9bf927fbc9dcb913f5f3c8bcd979b268bc
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-11-18 16:51:24 +0530

    added const variables

--- Variables/index.js
+++ Variables/index.js
@@ -1,56 +1,73 @@
-console.log(`Hello`);
-console.log(`I like Pizza!`);
+// console.log(`Hello`);
+// console.log(`I like Pizza!`);
 
-// ---------------------------------------------------------------
-// Display Alert
-// window.alert(`This is an alert!`);
-// window.alert(`I like Pizza!`);
+// // ---------------------------------------------------------------
+// // Display Alert
+// // window.alert(`This is an alert!`);
+// // window.alert(`I like Pizza!`);
 
-// ---------------------------------------------------------------
-// Change or add the element content text
-document.getElementById("head1").textContent = `Hello World`;
-document.getElementById("p1").textContent = `I like ice cream`;
+// // ---------------------------------------------------------------
+// // Change or add the element content text
+// document.getElementById("head1").textContent = `Hello World`;
+// document.getElementById("p1").textContent = `I like ice cream`;
 
-// ----------------------------------------------------------------
+// // ----------------------------------------------------------------
 
-// Variable = A container that stores a value
-//          = Behaves as if it were the value it contain
+// // Variable = A container that stores a value
+// //          = Behaves as if it were the value it contain
 
-// 1. declaration -> let x;
-// 2. assignment -> x = 100;
+// // 1. declaration -> let x;
+// // 2. assignment -> x = 100;
 
-// Number Data Types
-    let age = 25;
-    let price = 10.99;
-    let GPA = 2.4;
+// // Number Data Types
+//     let age = 25;
+//     let price = 10.99;
+//     let GPA = 2.4;
 
-    console.log(`Your age is ${age} years old`);
-    console.log(`The price is $${price}`);
-    console.log(`Your GPA is $(GPA)`);
-    // Find data types -> typeOf()
-    console.log(typeof GPA);
+//     console.log(`Your age is ${age} years old`);
+//     console.log(`The price is $${price}`);
+//     console.log(`Your GPA is $(GPA)`);
+//     // Find data types -> typeOf()
+//     console.log(typeof GPA);
 
-// String Data Types
-    let firstName = "Sachinda"
-    let lastName = "Bandara"
-    let email = "sachinda@gmail.com"
+// // String Data Types
+//     let firstName = "Sachinda"
+//     let lastName = "Bandara"
+//     let email = "sachinda@gmail.com"
 
-    console.log(`My name is ${firstName} ${lastName}`);
-    console.log(`My email is ${email}`);
-    // Find data types -> typeOf()
-    console.log(typeof email);
+//     console.log(`My name is ${firstName} ${lastName}`);
+//     console.log(`My email is ${email}`);
+//     // Find data types -> typeOf()
+//     console.log(typeof email);
 
-// Boolean Data Type
-    let online = true;
-    console.log(`Chamindu is online: ${online}`);
-    // Find data types -> typeOf()
-    console.log(typeof online); 
+// // Boolean Data Type
+//     let online = true;
+//     console.log(`Chamindu is online: ${online}`);
+//     // Find data types -> typeOf()
+//     console.log(typeof online); 
 
-// Example
-    let fullName ="Sachinda Bandara"
-    let studentAge = 25;
-    let isStudent = false;
+// // Example
+//     let fullName ="Sachinda Bandara"
+//     let studentAge = 25;
+//     let isStudent = false;
 
-    document.getElementById("p_1").textContent = `Your Name is ${fullName}`;
-    document.getElementById("p_2").textContent = `You are ${studentAge} years old`;
-    document.getElementById("p_3").textContent = `Enrolled : ${isStudent}`;
+//     document.getElementById("p_1").textContent = `Your Name is ${fullName}`;
+//     document.getElementById("p_2").textContent = `You are ${studentAge} years old`;
+//     document.getElementById("p_3").textContent = `Enrolled : ${isStudent}`;
+
+
+//-------------------------------------------------------------------------------------
+
+// Const
+// const variable that can not be changed
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// document.getElementById("mySubmit").onclick = function(){
+//     radius = document.getElementById("myText").value;
+//     radius = Number(radius);
+//     circumference = 2 * PI * radius;
+//     document.getElementById("myH3").textContent = circumference + "cm";
+// }
