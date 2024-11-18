f9370ac835fb3608e57a24f6b546a78b1d9e8c71
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-11-18 16:32:18 +0530

    Type conversion

---
+++ Type conversion/index.js
@@ -0,0 +1,65 @@
+// Type conversion = change the data type of a value to another ( String(), Number(), Boolean() )
+
+// let age = window.prompt("How old are you ?");
+// age = Number(age);
+// age += 1;
+// console.log(age, typeof age);
+
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+//----------------------------------------------------
+
+// let x = "0";
+// let y = "0";
+// let z = "0";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+//----------------------------------------------------
+
+
+// let x = "";
+// let y = "";
+// let z = "";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+//----------------------------------------------------
+
+
+// let x ;
+// let y ;
+// let z ;
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
